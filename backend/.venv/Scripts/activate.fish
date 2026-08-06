8d259fd611ce11b8f8cbe036d0ea05011a2c2bf6
Trevor Mikhala <trevormikhala194@gmail.com>
2026-08-06 13:39:51 +0300

    feat(events): complete event management setup

--- backend/.venv/Scripts/activate.fish
+++ backend/.venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\trevo\documents\project\TikitiHub-ticketing-platform\backend\.venv'
+set -gx VIRTUAL_ENV 'C:\Users\trevo\documents\TikitiHub-ticketing-platform\backend\.venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
